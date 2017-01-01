f4f8d121d1aaecdfc83efffef511a6f48b7e7845
WRQQQ <wrqqqrrr@rambler.ru>
2017-01-01 18:33:37 +0300

    Oop refactor app

--- js/oopmain.js
+++ js/oopmain.js
@@ -1,34 +1,57 @@
 (function(){
   'use strict';
-  var Todo = {}; //Model
+  var Todo = {
+    
+  }; //Model
   var Todos = function() {
     this.collection = [];
-    this.addTodo = function () {
-      this.collection.push(newTodo);
+    this.addTodo = function (todo) {
+      this.collection.push(todo);
     }
   }; //collection
 
-  var App = function () {
+  var App = function() {
     this.Todos = new Todos();
-
+    this.initEvents();
     return this; //chaining
   };
 
   App.prototype = {
-    addTodo: function () {
+    addTodo: function() {
       this.Todos.addTodo(object)
     },
-    render: function () {
-      this.Todos.collection.each()
+    render: function() {
+      var tmp = '';
+      var mass = '';
+      this.Todos.collection.forEach(function(elem, i, arr) {
+        mass += '<div data-index=' + i + '>' + elem.title + '</div>';
+
+      })
+      tmp = mass;
+      mass = '';
+      $('#list').html(tmp);
+    },
+    initEvents: function() {
+      // body... 
+      var _this = this;
+      $(document).on('click', '#btn', function(){return _this.makeTodo()})
+      .on('click', '#btn', function(e){return _this.todoText(e)})
+    },
+    makeTodo: function() {
+      this.Todos.addTodo({
+        title: $('input').val(),
+        resolved:false,
+        rend: false
+      });
+      console.log(this.Todos);
+      this.render();
+    },
+    todoText: function(e) {
+      console.log(e.currentTarget);
     }
   }
 
-  window.document.addEventListener('domContentLoaded', function  () {
+
     window.app = new App();
-  })
+
 })()
-document.getElementById(“addTodo”)..addEventListener("click", function(){
-  var todoText = document.getElementById(“todoText").value;
-   window.app.addTodo(todoText);
-   window.app.render();
-});
